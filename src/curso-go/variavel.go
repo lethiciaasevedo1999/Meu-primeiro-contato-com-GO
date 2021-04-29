e97178cced3f6135b43cc2df1bbacbb8f1d41e77
Lethicia Asevedo <lethiciaasevedo01@gmail.com>
2021-04-29 17:45:47 -0300

    Imprimindo variaveis

---
+++ src/curso-go/variavel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	a := "Wesley"
+	b := 10
+	c := 3.144
+	d := false
+	e := `uoooooouou legal`
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+
+	
+
+
+}
