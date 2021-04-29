e32ad9cb7abbd3c0316c29fc9c9bc4bbff85b6f7
Lethicia Asevedo <lethiciaasevedo01@gmail.com>
2021-04-29 18:30:45 -0300

    Declarando variaveis

--- src/curso-go/variavel.go
+++ src/curso-go/variavel.go
@@ -4,17 +4,23 @@ import "fmt"
 
 func main(){
 
-	a := "Wesley"
-	b := 10
-	c := 3.144
-	d := false
-	e := `uoooooouou legal`
+	//a := "Wesley"
+	//b := 10
+	//c := 3.144
+	//d := false
+	//e := `uoooooouou legal`
 
-	fmt.Printf("%v \n", a)
-	fmt.Printf("%v \n", b)
-	fmt.Printf("%v \n", c)
-	fmt.Printf("%v \n", d)
-	fmt.Printf("%v \n", e)
+	name := "Lethicia Yara do Nascimento Asevedo"
+	years := 22
+
+	fmt.Println("\n", name)
+	fmt.Println("\n", years)
+
+	//fmt.Printf("%v \n", a)
+	//fmt.Printf("%v \n", b)
+	//fmt.Printf("%v \n", c)
+	//fmt.Printf("%v \n", d)
+	//fmt.Printf("%v \n", e)
 
 	
 
